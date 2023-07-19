d8609038a6988a0869d701093a8ee96cbdb181a0
SiHyeon Lee <49221672+dsjk3172@users.noreply.github.com>
2023-07-19 16:42:15 +0900

    Update RetroInterface.kt

--- app/src/main/java/com/example/chat_app/RetroInterface.kt
+++ app/src/main/java/com/example/chat_app/RetroInterface.kt
@@ -50,7 +50,7 @@ interface RetroInterface{
 
 
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
-        private const val BASE_URL = "http://192.168.0.34:8080" //
+        private const val BASE_URL = "http://ip:port" //
 
         fun create(): RetroInterface {
             val gson : Gson =   GsonBuilder().setLenient().create();
